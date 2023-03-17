3953c81c24ed3fbfa5fd193a98d64384735763ba
Inye Allison <amazingfred4real@gmail.com>
2023-03-17 03:18:48 +0000

    Write a c code to print a given text and a new line with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - Entry point to the program
+ * Return: 1 will be returned
  */
 int main(void)
 {
 	int i = 0;
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	while (str[i] != "\0")
-	{
 
+	while (str[i]  != '\0')
+	{
 		putchar(str[i]);
 		n++;
-
 	}
-
 	return (1);
 }
