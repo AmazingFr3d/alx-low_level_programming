c880e1f1719b3e58104670cb9282b338dfc4d22f
Inye Allison <amazingfred4real@gmail.com>
2023-04-06 02:10:05 +0000

    Write a function that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - check the code
+ * *s: String to be printed
+ * Return: void.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
