ef8fc29ecb2d4e61af49c7a60169ad6d06fab11e
Inye Allison <amazingfred4real@gmail.com>
2023-04-07 04:22:05 +0000

    Corrections

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,22 +4,18 @@
  * print_most_numbers - a function that prints the numbers, from 0 to 9
  * Return: Void
  */
-
 void print_most_numbers(void)
 {
-	int i = 0;
-
-	while (i > 10)
+	int n;
+	
+	for (n = 0; n < 10; n++)
 	{
-		if (i == 2 || i == 4)
+		if (n == 2 || n == 4)
 		{
 			continue;
 		}
-		else
-		{
-			_putchar(i + '0');
-		}
-		i = i + 1;
+	    _putchar(n + '0');
 	}
 	_putchar('\n');
 }
+
