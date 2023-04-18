72d375d5f29441d7733a15e27c0043db44b0f7cf
Inye Allison <amazingfred4real@gmail.com>
2023-04-18 23:58:08 +0000

    Write a function that initialize a variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "dog.h"
+
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: dog struct pointer
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner's name
  * Return: Void
  */
-void init_dog(struct dog *d, char *name, float age, char *owner) {
-	if(d != NULL)
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
 	{
-		strncpy(d->name, name, sizeof(d->name) - 1);
-		d->name[sizeof(d->name) - 1] = '\0';
-		d->age = age;
-		strncpy(d->owner, owner, sizeof(d->owner) - 1);
-		d->owner[sizeof(d->owner) - 1] = '\0';
-	}
-	else
-	{
-		return;
+		d->name = malloc(strlen(name) + 1);
+		d->owner = malloc(strlen(owner) + 1);
+		if (d->name != NULL && d->owner != NULL)
+		{
+			strcpy(d->name, name);
+			d->age = age;
+			strcpy(d->owner, owner);
+		}
+		else
+		{
+			free(d->name);
+			free(d->owner);
+			printf("Memory allocation failed\n");
+			exit(EXIT_FAILURE);
+		}
 	}
 }
