288968c2e705738ec5cf36cc2fa46c410947884a
Inye Allison <amazingfred4real@gmail.com>
2023-04-20 15:46:22 +0000

    Write a program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
 /**
  * op_add - Adds 2 int values
@@ -37,8 +39,12 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-
-	return (a / b);
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	printf("Error\n");
+	exit(100);
 }
 /**
  * op_mod - modulus
@@ -48,5 +54,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	return (a % b);
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	printf("Error\n");
+	exit(100);
 }
