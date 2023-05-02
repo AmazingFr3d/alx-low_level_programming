2ddb9941ce20bf1add67ac1266ee082d9a093d70
Inye Allison <amazingfred4real@gmail.com>
2023-05-02 02:17:59 +0000

    Write a function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a list_t list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: None.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
