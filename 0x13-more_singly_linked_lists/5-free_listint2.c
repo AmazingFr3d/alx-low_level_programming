6e7851d2a4aa8ad67085b26f0d3853367f637e8b
Inye Allison <amazingfred4real@gmail.com>
2023-05-02 03:05:20 +0000

    Write a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 	{
 		return;
 	}
@@ -23,5 +23,6 @@ void free_listint2(listint_t **head)
 			*head = (*head)->next;
 			free(temp);
 		}
+		*head = NULL;
 	}
 }
